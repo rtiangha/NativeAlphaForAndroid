d8e29e01762138e882065a62c9a0d35310966940
Dmitry Rubtsov <me@mazy.wtf>
2026-01-24 17:59:51 +0600

    Enhance share button to use selected link URL

--- app/src/main/java/com/cylonid/nativealpha/WebViewActivity.java
+++ app/src/main/java/com/cylonid/nativealpha/WebViewActivity.java
@@ -114,6 +114,7 @@ public class WebViewActivity extends AppCompatActivity implements EasyPermission
     private String urlOnFirstPageload = "";
     private boolean fallbackToDefaultLongClickBehaviour = false;
     private PopupMenu mPopupMenu = null;
+    private String lastLongClickedLinkUrl = null;
 
     private AdFilter adFilter;
 
@@ -263,6 +264,15 @@ public class WebViewActivity extends AppCompatActivity implements EasyPermission
                 fallbackToDefaultLongClickBehaviour = false;
                 return false;
             }
+
+            WebView.HitTestResult result = wv.getHitTestResult();
+            if (result.getType() == WebView.HitTestResult.SRC_ANCHOR_TYPE ||
+                result.getType() == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
+                lastLongClickedLinkUrl = result.getExtra();
+            } else {
+                lastLongClickedLinkUrl = null;
+            }
+
             showWebViewPopupMenu();
             return true;
         });
@@ -442,7 +452,8 @@ public class WebViewActivity extends AppCompatActivity implements EasyPermission
         View center = findViewById(R.id.anchorCenterScreen);
         mPopupMenu = IconPopupMenuHelper.getMenu(center, R.menu.wv_context_menu, WebViewActivity.this);
 
-        String currentUrl = wv.getUrl();
+        boolean hasLink = lastLongClickedLinkUrl != null && !lastLongClickedLinkUrl.isEmpty();
+        String currentUrl = hasLink ? lastLongClickedLinkUrl : wv.getUrl();
         String title = "";
         if (currentUrl != null) {
             title = currentUrl.length() < 32 ? currentUrl : currentUrl.substring(0, 32) + "…";
@@ -463,9 +474,9 @@ public class WebViewActivity extends AppCompatActivity implements EasyPermission
             spanString.setSpan(foregroundColorSpan, 0, spanString.length(),0);
             item.setTitle(spanString);
         }
-        if(wv.canGoForward()) mPopupMenu.getMenu().getItem(2).setVisible(true);
+        if(wv.canGoForward()) mPopupMenu.getMenu().findItem(R.id.cmItemForward).setVisible(true);
         if(BuildConfig.DEBUG) {
-            mPopupMenu.getMenu().getItem(6).setVisible(true);
+            mPopupMenu.getMenu().findItem(R.id.cmShowAdblockProviders).setVisible(true);
         }
         mPopupMenu.setOnMenuItemClickListener(menuItem -> {
             switch(menuItem.getItemId()) {
@@ -487,7 +498,7 @@ public class WebViewActivity extends AppCompatActivity implements EasyPermission
                     new ShareCompat.IntentBuilder(WebViewActivity.this)
                             .setType("text/plain")
                             .setChooserTitle("Share URL")
-                            .setText(wv.getUrl())
+                            .setText(hasLink ? lastLongClickedLinkUrl : wv.getUrl())
                             .startChooser();
                     return true;
                 case R.id.cmItemCloseWebApp:
@@ -556,7 +567,7 @@ public class WebViewActivity extends AppCompatActivity implements EasyPermission
         wv.resumeTimers();
         this.setDarkModeIfNeeded();
 
-        
+
         if(webapp.isBiometricProtection()) {
             View fullActivityView = findViewById(R.id.webviewActivity);
             fullActivityView.setVisibility(View.GONE);
